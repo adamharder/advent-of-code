a903ee0ea799687abda5e222d00d943467880f2e
Adam Harder <complex653@gmail.com>
2024-12-03 04:11:46 +0000

    re-org

--- 2024/2.py
+++ 2024/2.py
@@ -22,24 +22,17 @@ def read_input():
         output.append(row)
     return output
 
-def info(msg, level=0):
-    if level > 0:
-        logger.info(msg)
 
-def error(msg, level=0):
-    if level > 0:
-        logger.error(msg)
-
-def is_safe(row, level=0):
+def get_error(row):
     if len(row)!=len(list(set(row))):
-        error(f"repeated value in {row}", level=level)
-        return False
+        return f"repeated value in {row}"
+
     if row[0] > row[-1]:
         # row is descending
         x=str(row)
         row.sort(reverse=True)
         if str(row) != x:
-            error(f"Descending row out of order: {x} -> {row}", level=level)
+            return f"Descending row out of order: {x} -> {row}"
             return False
 
     else:
@@ -47,44 +40,46 @@ def is_safe(row, level=0):
         x=str(row)
         row.sort(reverse=False)
         if str(row) != x:
-            error(f"Ascending row out of order: {x} -> {row}", level=level)
-            return False
+            return f"Ascending row out of order: {x} -> {row}"
     for n in range(len(row)-1):
         lhs = row[n]
         rhs = row[n+1]
         delta = abs(lhs - rhs)
         if delta > 3:
-            error(f"Rapid change (|{lhs}-{rhs}|={delta}) in {row}", level=level)
-            return False
-    #logger.debug(f"GOOD! {row}")
-    return True
+            return f"Rapid change (|{lhs}-{rhs}|={delta}) in {row}"
+
+    return None
+
+def is_safe(row, dampened=False):
+    error = get_error(row)
+    if error is None:
+        return True
+    if dampened:
+        for n in range(len(row)):
+            dampened_row = row.copy()
+            removed_value = dampened_row.pop(n)
+            #logger.debug(f"{n} REMOVED {removed_value}")
+            if is_safe(dampened_row, dampened=False):
+                logger.debug(f"Safe by removing the level {n} ({removed_value}): {row} -> {dampened_row} ")
+                return True
+
+        #logger.error(f"NEVER FOUND A SAFE VALUE {row}")
+    logger.error(error)
+    return False
+
 
 
 def part_1():
     input = read_input()
     return len([row for row in input if is_safe(row)])
 
-def is_safe_dampened(row):
-    if is_safe(row):
-        #logger.info(f"ALREADY SAFE: {row}")
-        return True
 
-    for n in range(len(row)):
-        dampened_row = row.copy()
-        removed_value = dampened_row.pop(n)
-        #logger.debug(f"{n} REMOVED {removed_value}")
-        if is_safe(dampened_row, level=0):
-            logger.debug(f"Safe by removing the level {n} ({removed_value}): {row} -> {dampened_row} ")
-            return True
-
-    #logger.error(f"NEVER FOUND A SAFE VALUE {row}")
-    return False
 
 
 
 def part_2():
     input = read_input()
-    return sum([1 for row in input if is_safe_dampened(row)])
+    return sum([1 for row in input if is_safe(row, dampened=True)])
 
 
 
